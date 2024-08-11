cc39f24add3d61d3c0ac3b62eb66beb8118ffb60
Rahul Sahni <rahul.sahni@altudo.co>
2024-08-11 22:01:01 +0530

    added test

---
+++ __test__/components/HeroBanner/data.ts
@@ -0,0 +1,29 @@
+export const dataHB = {
+  fields: {
+    BackgroundImage: {
+      value: {
+        src: 'http://citrin-app.dev.local/-/media/Brentwood-LA/image-5.jpg?h=688&iar=0&w=1920&hash=32A4FBC70E15ADEB4CACA64BA9924B00',
+        alt: '',
+        width: '1920',
+        height: '688',
+      },
+    },
+    CTA: {
+      value: {
+        href: '/',
+        text: 'Get ERP',
+        anchor: '',
+        linktype: 'internal',
+        class: '',
+        title: '',
+        querystring: '',
+        id: '{5A8A7983-FE1F-4B27-B38F-348A21F0EEFA}',
+      },
+    },
+    Description: {
+      value:
+        'Optimize business productivity while keeping costs low with an Enterprise Resource Planning (ERP) solution from FMT. Whether using Microsoft or Oracle NetSuite for your ERP foundation, FMT will enhance core financials, ensure data accuracy, boost supply chain visibility, and streamline your financial closing process—all while instilling confidence in your team.',
+    },
+    Title: { value: 'Practical ERP Solutions. Proven Results.' },
+  },
+};
